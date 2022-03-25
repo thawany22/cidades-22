35c9177a2e174f49148ea4e79f31380e9013b2d9
Thawany <souzaathaay4@gmail.com>
2022-03-25 19:33:40 -0300

    tarefa realizada

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Mongaguá");
+Console.WriteLine("Santos");
+Console.WriteLine("Guarujá");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Peruíbe");
